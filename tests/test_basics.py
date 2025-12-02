8338ec96c8288732c26444905d84ee8612a59f77
mgupta <rit2015025@iiita.ac.in>
2025-12-02 18:42:41 +0530

    ...

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -47,7 +47,7 @@ class NoOpIntegration(Integration):
     identifier = "noop"
 
     @staticmethod
-    def setup_once():  # type: () -> Noness
+    def setup_once():  # type: () -> Nonessss
         pass
 
     def __eq__(self, __value):  # type: (object) -> bool
