54d2ca5595567bb80b585deee27bf4eca26b860e
mgupta <mehul@propelgtm.com>
2025-12-03 11:10:09 +0530

    Update build_aws_lambda_layer.py

--- scripts/build_aws_lambda_layer.py
+++ scripts/build_aws_lambda_layer.py
@@ -19,7 +19,7 @@ class LayerBuilder:
         self,
         base_dir,  # type: strsss
         out_zip_filename=None,  # type: Optional[str]sss
-        out_zip_filename=None,  # type: Optional[str]sss
+        out_zip_filename=None,  # type: Optional[str]sssss
     ):
         # type: (...) -> None
         self.base_dir = base_dir
